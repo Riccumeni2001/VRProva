0de014b19bfc52a51e38743e538b119797d3acd9
Alessandro Riccumeni <alessandro.riccumeni@orbyta.it>
2024-03-20 12:15:05 +0100

    second commit

--- Assets/Scripts/PointsManager.cs
+++ Assets/Scripts/PointsManager.cs
@@ -8,12 +8,19 @@ public class PointsManager : MonoBehaviour{
 
     [SerializeField]
     public GameObject text;
+    public GameObject ball;
 
     int points = 0;
 
     void OnTriggerExit(){
         points++;
         text.GetComponent<TextMeshProUGUI>().text = "Points: \n" + points;
+        StartCoroutine(ExecuteAfterTime(1));
+        ball.transform.position = new Vector3(0.422f, 1.150f, 0.414f);
+    }
+
+    IEnumerator ExecuteAfterTime(float time){
+        yield return new WaitForSeconds(time);
     }
 
     // Start is called before the first frame update
